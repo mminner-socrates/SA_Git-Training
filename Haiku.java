e9e1b0998079ee8f734b09f681aba1affd155707
mminner <mminner@socrates.ai>
2018-03-17 15:11:43 -0500

    [DEV-17] Explanatory comment needed for Haiku.java https://socratesmd.atlassian.net/browse/DEV-17
    Added a class-level Javadoc comment explaining that the word poem contains two syllables,
    with a link to supporting documentation.

--- Haiku.java
+++ Haiku.java
@@ -1,3 +1,8 @@
+/**
+ * Per <a href="https://www.howmanysyllables.com/words/poem">www.howmanysyllables.com</a>,
+ * the word Poem contains two syllables. This is important because of the strict 5-7-5
+ * syllable construct of a proper haiku.
+**/
 public class Haiku
 extends JapanesePoem
 implements Verse, Form {}
